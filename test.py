5f3844fe1faf99aaeb4a8d8b6a532618518e7ac8
Ahsan Fazal <me@ahsanfazal.com>
2021-04-05 01:50:54 +0200

    Update

--- test.py
+++ test.py
@@ -7,32 +7,8 @@ import numpy as np
 # dimensions of our images
 img_width, img_height = 420, 280
 
-# load the model we saved
-model = load_model('./airline.h5')
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
+loaded_model = load_model('airline.h5')
 
-# predicting images
-img = image.load_img('./data-mining/images/train/KLM Royal Dutch Airlines.Boeing.737.1cc2cee8-2f6c-457b-96b7-e50c0cbd5f96.jpg',
-                     target_size=(img_width, img_height))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
+test = loaded_model.layers[0].input_shape  # (None, 160, 160, 3)
 
-images = np.vstack([x])
-classes = model.predict_classes(images, batch_size=10)
-print(classes)
-
-# # predicting multiple images at once
-# img = image.load_img('test2.jpg', target_size=(img_width, img_height))
-# y = image.img_to_array(img)
-# y = np.expand_dims(y, axis=0)
-
-# # pass the list of multiple images np.vstack()
-# images = np.vstack([x, y])
-# classes = model.predict_classes(images, batch_size=10)
-
-# # print the classes, the images belong to
-# print classes
-# print classes[0]
-# print classes[0][0]
+print(test)
